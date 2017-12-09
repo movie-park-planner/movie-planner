fab3c86ae1e9296655a9cb5118add00a42dde9df
Tyler Seybold <tycbold@gmail.com>
2017-12-09 13:16:05 -0600

    Adding to config and models folders

--- config/orm.js
+++ config/orm.js
@@ -66,8 +66,25 @@ var orm = {
       }
       cb(result);
     });
+  },
+  // An example of objColVals would be {name: panther, sleepy: true}
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
   }
 };
 
-// Export the orm object for the model (cat.js).
 module.exports = orm;
