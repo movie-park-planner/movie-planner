b11792ebaef5d2ee4a4615fdee215fdf9e9c235c
Tyler Seybold <tycbold@gmail.com>
2017-12-16 12:53:54 -0600

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -41,8 +41,8 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 var orm = {
-  all: function(tableOne, tableTwo, cb) {
-    var queryString = "SELECT * FROM " + tableOne + ", " + tableTwo + ";";
+  all: function(tableOne, cb) {
+    var queryString = "SELECT * FROM " + tableOne + ";";
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
