540bcfd9b2a9ac314e26d0e1d1e789f7824e9776
Tyler Seybold <tycbold@gmail.com>
2017-12-16 16:22:42 -0600

    Update moviesController.js

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -5,47 +5,14 @@ var router = express.Router();
 // Import the model to use its database functions.
 var movie = require("../models/planner-todo.js");
 
-// Create all our routes and set up logic within those routes where required.
-// router.get("/", function(req, res) {
-//   movie.all(function(data) {
-//     var hbsObject = {
-//       movies: data
-//     };
-//     console.log("Got all movies");
-//   movie.allComments(function(data) {
-//     var hbsObject2 = {
-//       comments: data
-//     };
-//     console.log("Got all comments");
-//     res.render("index", hbsObject, hbsObject2);
-//   });
-// });
-// });
-
-router.get("/", function(req, res) {
-  objectArray = [];
-
-  movie.all(function(data){
-    var hbsObject = data;
-    console.log ("got movies");
-    objectArray.push(hbsObject);
-  });
-
-  movie.allComments(function(data){
-    var hbsObject2 = data;
-    console.log("got comments");
-    objectArray.push(hbsObject2);
-  });
-
-  console.log(objectArray[0]);
-
-  var theObject = {
-      movies: objectArray[0],
-      comments: objectArray[1]
+router.get(“/”, function(req, res) {
+  movie.all(function(data) {
+    var hbsObject = {
+      moviescomments: data
     };
-
-  res.render("index", theObject);
-
+    console.log(hbsObject);
+    res.render(“index”, hbsObject);
+  });
 });
 
 router.get("/day", function(req, res) {
