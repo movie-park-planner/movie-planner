b3515c98f97220115edbd657238c57f63c623e6f
Tyler Seybold <tycbold@gmail.com>
2017-12-16 10:00:49 -0600

    Update moviesController.js

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model to use its database functions.
 var movie = require("../models/planner-todo.js");
 
 // Create all our routes and set up logic within those routes where required.
