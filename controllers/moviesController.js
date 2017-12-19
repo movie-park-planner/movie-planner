e95a901035b8e887b470e2108d6a4f53d0c8fe24
Tyler Seybold <tycbold@gmail.com>
2017-12-19 16:13:14 -0600

    Update moviesController.js

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -11,7 +11,7 @@ router.get("/", function(req, res) {
       moviescomments: data
     };
     console.log(hbsObject);
-    res.render(“index”, hbsObject);
+    res.render("index", hbsObject);
   });
 });
 
